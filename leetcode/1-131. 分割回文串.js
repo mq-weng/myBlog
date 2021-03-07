ca5cab458a58353cd992a51c676fca052ca0a17d
mq-weng <2223714315@qq.com>
2021-03-07 21:34:49 +0800

    if (isPalindrome(str)) {

---
+++ leetcode/1-131. 分割回文串.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function (s) {
+
+};
+let res = [];
+let arr = [];
+let str;
+function divide(s) {
+    for (let i = 0; i < s.length; i++) {
+        str = s.slice(0, i);
+        // isPalindrome(str) ? arr.push(str) : divide();
+        if (isPalindrome(str)) {
+            arr.push(str)
+            divide(s.slice(i, s.length))
+        }
+        else {
+            for (let j = i; j < s.length; j++) {
+                str = s.slice(i, j)
+                if (isPalindrome(str)) {
+                    arr.push(str)
+                    divide(s.slice(i, s.length))
+                }
+            }
+        }
+    }
+}
+
+function isPalindrome() {
+
+    return true
+}
