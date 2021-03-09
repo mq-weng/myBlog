4f000c6ef2dabfe827a11844ab960993edb6e38d
mq-weng <2223714315@qq.com>
2021-03-09 21:39:20 +0800

    existing probloms

--- leetcode/1-131. 分割回文串.js
+++ leetcode/1-131. 分割回文串.js
@@ -3,33 +3,37 @@
  * @return {string[][]}
  */
 var partition = function (s) {
-
+    divide(s);
+    return res;
 };
 let res = [];
 let arr = [];
-let str;
 function divide(s) {
-    for (let i = 0; i < s.length; i++) {
-        str = s.slice(0, i);
-        // isPalindrome(str) ? arr.push(str) : divide();
-        if (isPalindrome(str)) {
-            arr.push(str)
-            
+    for (let i = 1; i < s.length; i++) {
+        if (isPalindrome(s.slice(0, i))) {
+            arr.push(s.slice(0, i))
+            if (s.length == "") {
+                res.push(arr)
+            }
             divide(s.slice(i, s.length))
         }
         else {
-            for (let j = i; j < s.length; j++) {
-                str = s.slice(i, j)
-                if (isPalindrome(str)) {
-                    arr.push(str)
-                    divide(s.slice(i, s.length))
-                }
-            }
+            arr = []
         }
     }
+
 }
 
-function isPalindrome() {
-
+function isPalindrome(s) {
+    let i = 0;
+    let j = s.length;
+    while (i < j) {
+        if (s.charAt(i) != s.charAt(j)) {
+            return false
+        }
+        i++;
+        j--
+    }
     return true
 }
+console.log(partition("aab"))
