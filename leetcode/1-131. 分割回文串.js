7d4354ad50ec5e27210ba3b766b5df267dcf3383
mq-weng <2223714315@qq.com>
2021-03-13 15:51:10 +0800

    error

--- leetcode/1-131. 分割回文串.js
+++ leetcode/1-131. 分割回文串.js
@@ -2,38 +2,41 @@
  * @param {string} s
  * @return {string[][]}
  */
+let res = [];
+let arr = [];
+let num = 0;
 var partition = function (s) {
     divide(s);
     return res;
 };
-let res = [];
-let arr = [];
-function divide(s) {
-    for (let i = 1; i < s.length; i++) {
-        if (isPalindrome(s.slice(0, i))) {
+function divide(s) {//这个方法前面分完的保存不下来
+    for (let i = 1; i <= s.length; i++) {
+        if (isPalindrome(s.slice(0, i)) == true) {
+            console.log(s.slice(0, i))
             arr.push(s.slice(0, i))
-            if (s.length == "") {
-                res.push(arr)
-            }
             divide(s.slice(i, s.length))
         }
-        else {
-            arr = []
+        else{
+            continue
         }
+        if (i == s.length){
+            res.push(arr)
+        }
+        arr =[]
     }
 
 }
 
 function isPalindrome(s) {
     let i = 0;
-    let j = s.length;
+    let j = s.length - 1;
     while (i < j) {
         if (s.charAt(i) != s.charAt(j)) {
             return false
         }
         i++;
-        j--
+        j--;
     }
     return true
 }
-console.log(partition("aab"))
+console.log(partition("aabb"))
