e8817cd380a9edcf6d986fda8fc9be83d1801669
mq-weng <2223714315@qq.com>
2021-03-14 21:51:52 +0800

    2-349success

--- leetcode/2-349.两个数组的交集.js
+++ leetcode/2-349.两个数组的交集.js
@@ -9,24 +9,18 @@ var intersection = function (nums1, nums2) {
     for (var i = 0; i < nums1.length; i++) {
         for (var j = 0; j < nums2.length; j++) {
             if (nums1[i] == nums2[j]) {
-                // arr = deletRepeat(arr,nums1[i]);
-                for (let k = 0; k < arr.length; k++) {
-                    if (arr == []) {
-                        arr.push(nums1[i])
-                    }
-                    if (arr[k] != nums1[i]) {
-                        if (num == arr.length) {
-                            arr.push(nums1[i])
-                            console.log(arr);
-                            num = 0;
-                        }
-                        num++;
-                    }
-                }
+                arr = pushNoRepeat(arr,nums1[i]);
             }
         }
     }
     console.log(arr);
 };
+
+function pushNoRepeat(arr,num){
+    if(arr.indexOf(num) == -1){
+        arr.push(num)
+    }
+    return arr;
+}
 
 intersection([1, 2], [2, 2])
