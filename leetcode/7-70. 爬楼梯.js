2ecd91ca140f1a78090f765253bc35d26e2628ba
mq-weng <2223714315@qq.com>
2021-03-25 21:45:45 +0800

    爬楼梯

---
+++ leetcode/7-70. 爬楼梯.js
@@ -0,0 +1,16 @@
+var climbStairs = function (n) {
+    let floor1 = 1;
+    let floor2 = 2;
+    let num;
+    if (n <= 2) return n;
+    for (let i = 3; i <= n; i++) {
+        num = floor1 + floor2;
+        floor1 = floor2;
+        floor2 = num;
+    }
+    return num
+};
+
+
+
+console.log(climbStairs(27))
