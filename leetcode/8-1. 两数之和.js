9e47c47a0b0acb49db0cbd2f4dc882262fd405c9
mq-weng <2223714315@qq.com>
2021-03-26 15:03:04 +0800

    两数之和哈希

--- leetcode/8-1. 两数之和.js
+++ leetcode/8-1. 两数之和.js
@@ -10,4 +10,17 @@ var twoSum = function (nums, target) {
         }
     }
 };
-console.log(twoSum([3,3,3,4,7],6));
+
+//哈希
+var twoSum1 = function(nums, target) {
+    let map = new Map();
+    for(let i = 0, len = nums.length; i < len; i++){
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i];
+        }else{
+            map.set(nums[i], i);
+        }
+    }
+    return [];
+};
+console.log(twoSum1([3,3,3,4,7],6));
