562b7ca79ddb677e5026beb5f1e8900f9631fc79
mq-weng <2223714315@qq.com>
2021-03-29 09:38:56 +0800

    generator生成器

---
+++ 考前复习呀/4.generator.js
@@ -0,0 +1,22 @@
+function* next_id(){
+    let id = 0;
+    while (id < 6) {
+        yield id++
+    }
+}
+
+//方法1
+let ne = next_id();
+console.log(ne.next())
+console.log(ne.next())
+console.log(ne.next())
+console.log(ne.next())
+console.log(ne.next())
+console.log(ne.next())
+console.log(ne.next())
+console.log(next_id().next())//0  不能这样写,这样写相当于重复调用next_id(),归零了
+
+//方法2
+for (item of next_id()){
+    console.log(item)
+}
