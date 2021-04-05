a81e034d69643382a60dd97c92ab5dceff530c6e
mq-weng <2223714315@qq.com>
2021-04-05 10:16:12 +0800

    原型

---
+++ 考前复习呀/7.原型与class.js
@@ -0,0 +1,66 @@
+/* 1 */
+function Cat(name) {  //new出对象的（即是kitty） constructor，由kitty 用__proto__指出来的
+    this.name = name;//构造函数
+}
+Cat.prototype.say = function () { //prototype
+    console.log("Hello, " + this.name + "!")
+    return "Hello, " + this.name + "!";
+}
+// 测试:
+var kitty = new Cat('Kitty');
+var doraemon = new Cat('哆啦A梦');
+if (kitty && kitty.name === 'Kitty'
+    && kitty.say
+    && typeof kitty.say === 'function'
+    && kitty.say() === 'Hello, Kitty!'
+    && kitty.say === doraemon.say
+) {
+    console.log('测试通过!');
+} else {
+    console.log('测试失败!');
+}
+console.log(kitty.__proto__)//{ say: [Function (anonymous)] }
+console.log(kitty.constructor) //[Function: Cat]
+console.log(Cat.prototype)//{ say: [Function (anonymous)] }
+
+//原型继承
+//直接这么写会破坏原型链
+function Hua(props) { 
+    Cat.call(this, props.name)
+    // name: props.name;
+    age: props.age;
+}
+Hua.prototype = Cat.prototype
+let k = new Hua({name:"hh", age: 20})
+console.log(k.say())
+console.log(k instanceof Cat) //验证继承
+console.log(k instanceof Hua)
+console.log(k.__proto__ === Hua.prototype) //true  //验证原型
+console.log(k.__proto__.__proto__ === Cat.prototype)//false
+
+
+
+
+
+
+
+//class继承
+class Student {
+    constructor(name) {
+        this.name = name;
+    }
+
+    hello() {
+        alert('Hello, ' + this.name + '!');
+    }
+}
+class PrimaryStudent extends Student {
+    constructor(name, grade) {
+        super(name); // 记得用super调用父类的构造方法!
+        this.grade = grade;
+    }
+
+    myGrade() {
+        alert('I am at grade ' + this.grade);
+    }
+}
