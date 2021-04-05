78d68f88bcfda4120219894b263d2b878ba85307
mq-weng <2223714315@qq.com>
2021-04-05 10:49:19 +0800

    原型继承

--- 考前复习呀/7.原型与class.js
+++ 考前复习呀/7.原型与class.js
@@ -23,20 +23,27 @@ console.log(kitty.__proto__)//{ say: [Function (anonymous)] }
 console.log(kitty.constructor) //[Function: Cat]
 console.log(Cat.prototype)//{ say: [Function (anonymous)] }
 
-//原型继承
-//直接这么写会破坏原型链
+//原型继承（两种方法）原型链如下图
+//new Hua() ----> Hua.prototype ----> Cat.prototype ----> Object.prototype ----> null
 function Hua(props) { 
     Cat.call(this, props.name)
     // name: props.name;
     age: props.age;
 }
-Hua.prototype = Cat.prototype
+// Hua.prototype = Cat.prototype  //直接这么写会破坏原型链,使Hua和Cat同级了
+function inherits(Child, Parent) {
+    Child.prototype = new Parent();//1
+    // Child.prototype = Object.create(Parent.prototype);//2
+    Child.prototype.constructor = Child 
+}
+// inherits(Hua, Cat)
+inherits1(Hua, Cat)
 let k = new Hua({name:"hh", age: 20})
 console.log(k.say())
 console.log(k instanceof Cat) //验证继承
 console.log(k instanceof Hua)
 console.log(k.__proto__ === Hua.prototype) //true  //验证原型
-console.log(k.__proto__.__proto__ === Cat.prototype)//false
+console.log(k.__proto__.__proto__ === Cat.prototype)//true
 
 
 
