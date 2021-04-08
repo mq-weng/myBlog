8601df849648e69f43033ce57023465400d735a1
mq-weng <2223714315@qq.com>
2021-04-08 21:28:24 +0800

    优化

--- leetcode/17-242. 有效的字母异位词.js
+++ leetcode/17-242. 有效的字母异位词.js
@@ -1,13 +1,21 @@
 //暴力，排序，验证相等
-var isAnagram = function(s, t) {
-    if (s.length != t.length) return false;
-    s = s.split("");
-    t = t.split("");
-    s.sort();
-    t.sort();
-    s = s.toString();
-    t = t.toString();
-    return s == t
+//时间复杂度O(nlogn) 
+//排序的时间复杂度为 O(nlog⁡n)
+var isAnagram1 = function(s, t) {
+    return t.length == s.length && [...s].sort().join("") == [...t].sort().join("")
 };
 
 //哈希表
+//时间复杂度：O(n)，其中 n 为 s 的长度。
+var isAnagram = function(s, t) {
+    let hash = {};
+    for ({char,index} in s) {
+        hash[char] = index
+    }
+    console.log(char)
+};
+
+
+console.log(isAnagram1("huahua","auhu")) //false
+console.log(isAnagram1("huahua","auhahu")) //true
+isAnagram("huahua","auhahu")
