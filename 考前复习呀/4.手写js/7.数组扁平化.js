1b1564224e6448c3e2e08d52f41b9c49aacc3d29
mq-weng <2223714315@qq.com>
2021-04-21 21:39:12 +0800

    flat

--- 考前复习呀/4.手写js/7.数组扁平化.js
+++ 考前复习呀/4.手写js/7.数组扁平化.js
@@ -1,11 +1,17 @@
+
 let arr = [1,2,2,[2,3,4,5,[1,1,2]]];
-console.log(arr.toString())
+console.log(arr.toString().split(',').map((x) => {return parseInt(x)}))  //1.转为字符串
+console.log(arr.flat(Infinity))  //flat(嵌套层数) Infinity：所有层都去掉  //2. flat
 
-
-function test(arr) {
-    arr.forEach(element => {
-        element
-    });
-    return arr;
+function test(arr) {       //3.递归
+    let res = [];
+    let toArr = function (arr) {
+        arr.forEach(element => {
+            element instanceof Array ? toArr(element) : res.push(element)
+        });
+    }
+    toArr(arr)
+    return res;
 }
+
 console.log(test(arr))
