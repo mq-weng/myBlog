40b4e2dc98394b7f6ed4a10f624dca92dbd4fef1
mq-weng <2223714315@qq.com>
2021-06-03 16:10:07 +0800

    深度优先遍历

---
+++ 代码题/8.深度遍历.js
@@ -0,0 +1,32 @@
+let root = {
+    val: 1,
+    left:
+    {
+        val: 2,
+        left: {
+            val: 3
+        },
+        right:{
+            val: 4
+        }
+    },
+    right:
+    {
+        val: 5
+    }
+}
+
+function deep(root) {
+    let res = []
+    let d = (r) => {
+        if (!r) return res;
+        if (r.val) res.push(r.val);
+        if (r.left) d(r.left);
+        if (r.right) d(r.right);
+    }
+    d(root);
+    let re = res;
+    res = null;
+    return re
+}
+console.log(deep(root));
